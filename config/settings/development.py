0b9dc1f779c755597bd0d4597a26f5e7c9f49b7e
Eyob Tariku <eyobtariku@gmail.com>
2021-04-19 01:38:54 +0300

    Add AWS host url for development environment

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,11 @@ from .base import *
 
 
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '.dukka-api-dev.us-east-1.elasticbeanstalk.com',
+    '.localhost',
+    '.127.0.0.1'
+]
 ENVIRONMENT = 'dev'
 
 INSTALLED_APPS += [
