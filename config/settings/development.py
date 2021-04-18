aab46b001a4f7c2ccca3bcdec05c18c9e3f307bc
Eyob Tariku <eyobtariku@gmail.com>
2021-04-19 02:22:07 +0300

    Change the WSGI path in .ebextensions

--- config/settings/development.py
+++ config/settings/development.py
@@ -3,11 +3,12 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = [
-    '.dukka-api-dev.us-east-1.elasticbeanstalk.com',
-    '.localhost',
-    '.127.0.0.1'
+    'localhost',
+    '127.0.0.1',
+    'dukka-api-dev.us-east-1.elasticbeanstalk.com'
 ]
-ENVIRONMENT = 'dev'
+ENVIRONMENT = 'development'
+
 
 INSTALLED_APPS += [
     'django_extensions',
